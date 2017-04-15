03a20b630ee77c00e84f7ee2c2d3a2ae402437c8
Jannis R <mail@jannisr.de>
2017-04-15 13:15:32 +0200

    2017-04-10, compress when uploading

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -rav --exclude '.*' --exclude deploy.sh . uberspace:~/html/vbb-gtfs.jannisr.de/
+rsync -ravz --exclude '.*' --exclude deploy.sh . uberspace:~/html/vbb-gtfs.jannisr.de/
