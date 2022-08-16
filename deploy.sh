a894f6b877f0bf035b4acd02793e2f244919b33b
Jannis R <mail@jannisr.de>
2022-08-16 13:58:02 +0200

    minor tweaks

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-rsync -rvz --checksum --exclude '.*' --exclude deploy.sh . elm:/var/www/vbb-gtfs.jannisr.de/
+set -e
+set -x
+
+rsync -rvz --checksum \
+	--exclude '.*' --exclude deploy.sh --exclude lib --exclude build-feed.js \
+	. elm:/var/www/vbb-gtfs.jannisr.de/
